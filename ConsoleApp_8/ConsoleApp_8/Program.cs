d79986f501f763c8d63c84d78a8d0ed4d5ca5898
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2022-12-16 09:38:18 +0300

    Add files via upload

---
+++ ConsoleApp_8/ConsoleApp_8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            var str = Console.ReadLine();
+            var pattern = "(?<hour>0[0-9]|1[0-9]|2[0-3]):(?<min>[0-5][0-9]):(?<sec>[0-5][0-9])";
+            str = Regex.Replace(str, pattern, (m) =>
+            {
+                var ts = TimeSpan.Parse(m.Value);
+                if (ts.Seconds >= 30)
+                    ts = ts.Add(new TimeSpan(0, 1, 0));
+
+                return ts.ToString(@"hh\:mm");
+            });
+
+            Console.Write($"Измененная строка: {str}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp_8_form/ConsoleApp_8_form/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp_8_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            var str = textBox1.Text;
+            var pattern = "(?<hour>0[0-9]|1[0-9]|2[0-3]):(?<min>[0-5][0-9]):(?<sec>[0-5][0-9])";
+            str = Regex.Replace(str, pattern, (m) =>
+            {
+                var ts = TimeSpan.Parse(m.Value);
+                if (ts.Seconds >= 30)
+                    ts = ts.Add(new TimeSpan(0, 1, 0));
+
+                return ts.ToString(@"hh\:mm");
+            });
+
+            textBox2.Text += str;
+        }
+    }
+}
